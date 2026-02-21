7c4bb0a535c461f23a532f29046af4cca3c2f988
tenextractor <139619642+tenextractor@users.noreply.github.com>
2026-02-21 09:32:09 +0530

    fix: uppercase maps accidentally commented out by formatter

--- lib/src/main/kotlin/com/tenextractor/vietime/Common.kt
+++ lib/src/main/kotlin/com/tenextractor/vietime/Common.kt
@@ -67,21 +67,34 @@ object Common {
 
     /** A map of characters without accent to character with circumflex accent */
     public val CIRCUMFLEX_MAP =
-            mapOf( 'a' to 'â', 'e' to 'ê', 'o' to 'ô', // uppercase 'A' to 'Â', 'E' to 'Ê', 'O' to 'Ô',
+            mapOf(
+                'a' to 'â',
+                'e' to 'ê',
+                'o' to 'ô',
+                // uppercase
+                'A' to 'Â',
+                'E' to 'Ê',
+                'O' to 'Ô',
             )
 
     /** A map of characters without accent to character with dyet (D WITH STROKE) accent */
-    public val STROKE_MAP =
-            mapOf( 'd' to 'đ', 'D' to 'Đ',
-            )
+    public val STROKE_MAP = mapOf( 'd' to 'đ', 'D' to 'Đ')
 
     /** A map of characters without accent to character with horn accent */
     public val HORN_MAP =
-            mapOf( 'u' to 'ư', 'o' to 'ơ', // uppercase 'U' to 'Ư', 'O' to 'Ơ',
+            mapOf(
+                'u' to 'ư',
+                'o' to 'ơ',
+                // uppercase
+                'U' to 'Ư',
+                'O' to 'Ơ',
             )
 
     /** A map of characters without accent to character with breve accent */
     public val BREVE_MAP =
-            mapOf( 'a' to 'ă', // uppercase 'A' to 'Ă',
+            mapOf(
+                'a' to 'ă',
+                // uppercase
+                'A' to 'Ă',
             )
 }
